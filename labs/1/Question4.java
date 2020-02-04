089b8eb31ebb1917a370b467bcad77dd602921f3
michaelwise12 <Michael.Wise1@edu>
2020-02-04 17:53:09 -0500

    Fixed data type problems for Lab #1.

--- labs/1/Question4.java
+++ labs/1/Question4.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Question4 {
 
 	public static void main(String[] args) {
-		double speedoflight = 299972458;
+		final int speedoflight = 299972458;
 		
 		// Create scanner
 		Scanner input = new Scanner(System.in);
